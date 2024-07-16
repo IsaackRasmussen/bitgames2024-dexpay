b5486e51b49dc6c9aa788dc3aeb33d8e1a121308
Isaack Rasmussen <isaack@gmail.com>
2024-07-16 21:04:29 +0200

    Fixed Vercel.json

--- dexpay-web/server/src/main.ts
+++ dexpay-web/server/src/main.ts
@@ -21,8 +21,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+module.exports = app;
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
