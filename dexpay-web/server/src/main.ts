6f8fd658ad7bc8f5d1a6019b5ba7911aeeebafdc
Isaack Rasmussen <isaack@gmail.com>
2024-07-16 17:55:50 +0200

    Updated config

--- dexpay-web/server/src/main.ts
+++ dexpay-web/server/src/main.ts
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
