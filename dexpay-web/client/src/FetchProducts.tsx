9a634eb51c5eabbc863c9e4033de685c023b34bc
Isaack Rasmussen <isaack@gmail.com>
2024-07-16 21:26:22 +0200

    Fixed: API path

--- dexpay-web/client/src/FetchProducts.tsx
+++ dexpay-web/client/src/FetchProducts.tsx
@@ -3,15 +3,25 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { Button, CardActionArea, CardActions, Divider, Rating } from "@mui/material";
+import {
+  Button,
+  CardActionArea,
+  CardActions,
+  Divider,
+  Rating,
+} from "@mui/material";
 import { AddShoppingCart } from "@mui/icons-material";
+import { defineConfig, loadEnv } from "vite";
 
 const FetchProducts = () => {
+  const envars = loadEnv(env.mode, "./");
+  const serverAPIPath = envars.VITE_SERVER_API_PATH ?? "/api";
+
   const [products, setProducts] = useState([]);
   //const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    fetch(`/api/products/all`)
+    fetch(`${serverAPIPath}/products/all`)
       .then((res) => {
         return res.json();
       })
@@ -23,14 +33,14 @@ const FetchProducts = () => {
   }, []);
   return (
     <div>
-              <Button
-                size="small"
-                color="primary"
-                startIcon={<AddShoppingCart />}
-                href="lightning:lnbc20m1pvjluezpp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqhp58yjmdan79s6qqdhdzgynm4zwqd5d7xmw5fk98klysy043l2ahrqsfpp3qjmp7lwpagxun9pygexvgpjdc4jdj85fr9yq20q82gphp2nflc7jtzrcazrra7wwgzxqc8u7754cdlpfrmccae92qgzqvzq2ps8pqqqqqqpqqqqq9qqqvpeuqafqxu92d8lr6fvg0r5gv0heeeqgcrqlnm6jhphu9y00rrhy4grqszsvpcgpy9qqqqqqgqqqqq7qqzqj9n4evl6mr5aj9f58zp6fyjzup6ywn3x6sk8akg5v4tgn2q8g4fhx05wf6juaxu9760yp46454gpg5mtzgerlzezqcqvjnhjh8z3g2qqdhhwkj"
-              >
-                Order
-              </Button>
+      <Button
+        size="small"
+        color="primary"
+        startIcon={<AddShoppingCart />}
+        href="lightning:lnbc20m1pvjluezpp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqhp58yjmdan79s6qqdhdzgynm4zwqd5d7xmw5fk98klysy043l2ahrqsfpp3qjmp7lwpagxun9pygexvgpjdc4jdj85fr9yq20q82gphp2nflc7jtzrcazrra7wwgzxqc8u7754cdlpfrmccae92qgzqvzq2ps8pqqqqqqpqqqqq9qqqvpeuqafqxu92d8lr6fvg0r5gv0heeeqgcrqlnm6jhphu9y00rrhy4grqszsvpcgpy9qqqqqqgqqqqq7qqzqj9n4evl6mr5aj9f58zp6fyjzup6ywn3x6sk8akg5v4tgn2q8g4fhx05wf6juaxu9760yp46454gpg5mtzgerlzezqcqvjnhjh8z3g2qqdhhwkj"
+      >
+        Order
+      </Button>
 
       {products.map((product: any) => (
         <div key={product.id} style={{ paddingBottom: "2em" }}>
@@ -52,7 +62,7 @@ const FetchProducts = () => {
               </CardContent>
             </CardActionArea>
             <CardActions>
-            <Rating defaultValue={1} size="small" />
+              <Rating defaultValue={1} size="small" />
               <Button
                 size="small"
                 color="primary"
